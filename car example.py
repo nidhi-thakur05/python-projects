851cb599381720515755b6426cab8db4381e74ad
nidhi-thakur05 <nsthakur0511@gmail.com>
2024-07-12 23:50:32 +0530

    Add files via upload

---
+++ car example.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(self, make, model, year, speed):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.speed = speed
+
+    def honk(self):
+        print("Beep beep!")
+
+    def accelerate(self, rate):
+        self.speed += rate
+
+    def get_speed(self):
+        return self.speed
+
+
+my_car = Car("Toyota", "Camry", 2020, 7)
+my_car.honk()  # Output: "Beep beep!"
+my_car.accelerate(25)
+print(my_car.get_speed())  # Output: 30
+
